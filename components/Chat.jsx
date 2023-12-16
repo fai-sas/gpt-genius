cfa5f60b9b064e41540126aa82460974fd74d49c
fai-sas <craftedbyfaisal@gmail.com>
2023-12-16 21:55:19 +0600

    open ai api added

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -1,4 +1,5 @@
 'use client'
+
 import { generateChatResponse } from '@/utils/actions'
 import { useMutation } from '@tanstack/react-query'
 import { useState } from 'react'
@@ -8,12 +9,25 @@ const Chat = () => {
   const [text, setText] = useState('')
   const [messages, setMessages] = useState([])
   const { mutate } = useMutation({
-    mutationFn: (message) => generateChatResponse(message),
+    mutationFn: (query) => generateChatResponse([...messages, query]),
+
+    onSuccess: (data) => {
+      if (!data) {
+        toast.error('Something went wrong...')
+        return
+      }
+      setMessages((prev) => [...prev, data])
+    },
+    onError: (error) => {
+      toast.error('Something went wrong...')
+    },
   })
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    mutate(text)
+    const query = { role: 'user', content: text }
+    mutate(query)
+    setMessages((prev) => [...prev, query])
+    setText('')
   }
 
   return (
