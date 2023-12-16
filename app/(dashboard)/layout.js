b267c016f3d4b61588cafaeb049025227b9973bd
fai-sas <craftedbyfaisal@gmail.com>
2023-12-16 17:44:33 +0600

    sidebar added

--- app/(dashboard)/layout.js
+++ app/(dashboard)/layout.js
@@ -1,4 +1,29 @@
+import Sidebar from '@/components/Sidebar'
+import { FaBarsStaggered } from 'react-icons/fa6'
+
 const layout = ({ children }) => {
-  return <div>{children}</div>
+  return (
+    <div className='drawer lg:drawer-open'>
+      <input id='my-drawer-2' type='checkbox' className='drawer-toggle' />
+      <div className='drawer-content'>
+        {/* Page content here */}
+        <label
+          htmlFor='my-drawer-2'
+          className='fixed drawer-button lg:hidden top-6 right-6'
+        >
+          <FaBarsStaggered className='w-8 h-8 text-primary' />
+        </label>
+        <div className='min-h-screen px-8 py-12 bg-base-200'>{children}</div>
+      </div>
+      <div className='drawer-side'>
+        <label
+          htmlFor='my-drawer-2'
+          aria-label='close sidebar'
+          className='drawer-overlay'
+        ></label>
+        <Sidebar />
+      </div>
+    </div>
+  )
 }
 export default layout
